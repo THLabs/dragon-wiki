ef4a9a9a545b7e3728a0020eeda76e3d8f871592
Tom Haskell <tom@thlabs.net>
2015-08-10 10:43:12 +0100

    using requirejs for script loading

--- public/scripts/dragon.js
+++ public/scripts/dragon.js
@@ -1,1 +1,19 @@
-hljs.initHighlightingOnLoad();
+require.config({
+    paths: {
+        highlight: '/scripts/highlight.pack'
+    },
+    shim: {
+        'highlight': {
+            exports: 'hljs'
+        }
+    }
+});
+
+require([
+    "highlight"
+], function (hljs) {
+    'use strict';
+
+    hljs.initHighlighting();
+
+});
